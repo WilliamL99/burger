33d63f39b9730ebe877e2819a0c88b73fa00586c
William Lan <wlan76@yahoo.com>
2019-01-14 16:35:51 -0800

    hw upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES("chicken Burger", false),
+("Triple Burger", false),
+("Protein Style Burger", false),
+("Grilled cheese Burger", false),
+("Extra-toasted Burger", false);
